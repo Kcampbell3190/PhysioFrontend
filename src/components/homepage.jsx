92b8ceb685d28846e1430c408b75c51711ba6865
= <kcampbell3190@gmail.com>
2023-01-30 08:01:28 +1100

    routes

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,15 +1,41 @@
 import React from 'react'
 import './homepage.css'
+import { Routes, Route, Link } from "react-router-dom"
+import Account from './account'
+import Progress from './progress'
+import Programs from './programs'
 
 const homepage = () => {
   return (
    <>
     <div className='background'>
-      <h1>Welcome to the homepage</h1>
-    </div>
+      <h1>Welcome to your Homepage</h1>
+      </div>
+      
+    <Link to="/Account">
+    <button>Account</button>
+    </Link>
+
+    <Link to="/Progress">
+    <button>Progress</button>
+      </Link>
+      
+     <Link to="/Programs">
+    <button>Programs</button>
+    </Link>
+      
+
    
    
-    <h1>homepage</h1>
+      <h1>homepage</h1>
+      
+    <Routes>
+    <Route path="/Account" element={<Account />} />
+   
+    <Route path="/Progress" element={<Progress />} />
+        
+    <Route path="/Programs" element={<Programs />} />
+    </Routes>
 
   </>
   )
