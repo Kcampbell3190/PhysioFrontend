5e0de01f0d918351057cf3268c54ca3a99ae319e
kcampbell3190 <kcampbell3190@gmail.com>
2023-01-31 12:26:26 +1100

    added images and auth signup route'

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,49 @@
-import React from 'react'
+import React, { Component} from 'react'
 import { useState } from 'react'
 import './App.css'
 import Login from './components/Login'
 import Signup from './components/SignUp'
-import HomePage from './components/HomePage'
+import Yoga1 from './components/Images/yoga1.png'
+import Yoga2 from './components/Images/yoga2.png'
+import Yoga3 from './components/Images/yoga3.png'
+
+
 import { Routes, Route, Link} from "react-router-dom"
 
 
 
 function App() {
-  
 
-  return (
-    <>
-    {/* // <div className="background">
-    //   <div>LANDING PAGE</div> */}
-
-  
-
-    {/* <Link to="/">
-    </Link>
+ return (
     
-    <Link to="/Signup">
+     <>
+      <div>LANDING PAGE</div>
+      <img src={Yoga1}></img>
+      <img src={Yoga2}></img>
+      <img src={Yoga3}></img>
+
+  
+
+  
+    
+    <Link to="/auth/Signup">
     <button>Sign Up</button>
     </Link>
 
     <Link to="/Login">
     <button>Login</button>
-    </Link> */}
+    </Link> 
 
       <Routes>
-          <Route path="/" element={<HomePage />} />
+         
           <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/auth/signup" element={<Signup />} />
       </Routes>
     
+      </>
+    
 
-    </>
-
-  )
+   )
 }
 
 export default App
